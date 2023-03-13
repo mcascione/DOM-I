0f90b7f6cb9d733304274a87687e1df24b9c09de
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-13 17:48:20 -0400

    completed mvp

--- src/index.js
+++ src/index.js
@@ -39,4 +39,86 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+//Header Section
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks[0].textContent = 'Services';
+navLinks[1].textContent = 'Product';
+navLinks[2].textContent = 'Vision';
+navLinks[3].textContent = 'Features';
+navLinks[4].textContent = 'About';
+navLinks[5].textContent = 'Contact';
+navLinks.forEach(link => {
+  link.className = 'italic';
+});
+
+//Call to Action Section
+
+const ctaHeader = document.querySelector('.cta .cta-text h1');
+ctaHeader.textContent = 'DOM Is Awesome';
+
+const ctaButton = document.querySelector('.cta .cta-text button');
+ctaButton.textContent = 'Get Started';
+
+//Main section
+
+const topContentHeaders = document.querySelectorAll('.top-content .text-content h4');
+topContentHeaders[0].textContent = 'Features';
+topContentHeaders[1].textContent = 'About';
+
+//main top content 
+const topContentParagraphs = document.querySelectorAll('.top-content .text-content p');
+
+topContentParagraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+topContentParagraphs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//main bottom content
+const bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4');
+bottomContentHeaders[0].textContent = 'Services';
+bottomContentHeaders[1].textContent = 'Product';
+bottomContentHeaders[2].textContent = 'Vision';
+
+const bottomContentParagraphs = document.querySelectorAll('.bottom-content .text-content p');
+bottomContentParagraphs[0].textContent =  "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+bottomContentParagraphs[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+bottomContentParagraphs[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//Footer Section
+const contactHeader = document.querySelector('.contact h4')
+contactHeader.textContent = 'Contact';
+
+const address = document.querySelector('.contact p:nth-of-type(1)');
+address.textContent = '123 Way 456 Street Somewhere, USA';
+
+const phone = document.querySelector('.contact p:nth-of-type(2)');
+phone.textContent = '1 (888) 888-8888';
+
+const email = document.querySelector('.contact p:nth-of-type(3)');
+email.textContent = 'sales@greatidea.io';
+
+const copyright = document.querySelector('footer a');
+copyright.className = "bold";
+copyright.textContent = 'Copyright Great Idea! 2021';
+
+//images
+const logoImg = document.querySelector('#logo-img');
+logoImg.setAttribute('src', 'http://localhost:9000/img/logo.png');
+console.log(logoImg);
+
+const ctaImg = document.querySelector('#cta-img')
+ctaImg.setAttribute('src', 'http://localhost:9000/img/cta.png');  
+
+const accentImg = document.querySelector('#middle-img');
+accentImg.setAttribute('src', 'http://localhost:9000/img/accent.png');
+
+
+
+
+
+
+
+
+
